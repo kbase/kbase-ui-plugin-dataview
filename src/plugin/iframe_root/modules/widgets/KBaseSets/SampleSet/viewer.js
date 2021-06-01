7cdad7e5a5f0bbfa6f75d1c814692143cead7a99
Erik Pearson <eapearson@lbl.gov>
2021-06-01 07:13:44 -0700

    SampleSet: add updated format definitions, minor code reorg.

--- src/plugin/iframe_root/modules/widgets/KBaseSets/SampleSet/viewer.js
+++ src/plugin/iframe_root/modules/widgets/KBaseSets/SampleSet/viewer.js
@@ -99,7 +99,7 @@ define([
          * @returns {Promise<*[]>}
          */
         async samplesToTable(model, samples, sampleSet, format, allFieldKeys) {
-            const fieldGroups = await model.getJSON('mock-data/groups/groups');
+            const fieldGroups = await model.getFieldGroups();
             const fieldKeys = new Set(Array.from(allFieldKeys));
 
             // first pass, just flatten out all the fields, and pluck out the ones in fieldKeys.
