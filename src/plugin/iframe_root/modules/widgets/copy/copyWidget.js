d97109eeb3fb4693d968cf0b338280bb52f9e9bf
Erik Pearson <eapearson@lbl.gov>
2022-05-25 14:32:17 -0700

    refactor to inject narrative title as text not html [UFI-16]

--- src/plugin/iframe_root/modules/widgets/copy/copyWidget.js
+++ src/plugin/iframe_root/modules/widgets/copy/copyWidget.js
@@ -107,23 +107,8 @@ define([
                                     narrativeInfo.workspaceInfo.id,
                                     narrativeInfo.workspaceInfo.metadata.narrative
                                 )}`
-                        ),
-                        message = div([
-                            'Successfully copied this data object to the Narrative ',
-                            narrativeInfo.workspaceInfo.metadata.narrative_nice_name,
-                            span({style: {fontStyle: 'italic'}}, [
-                                a(
-                                    {
-                                        href: narrativeUrl,
-                                        class: 'btn btn-default',
-                                        target: '_blank'
-                                    },
-                                    'Open this Narrative'
-                                )
-                            ])
-                        ]);
-
-                    viewModel.completionMessage(message);
+                        );
+                    viewModel.completionMessage({title: narrativeInfo.workspaceInfo.metadata.narrative_nice_name, url: narrativeUrl});
                     return copiedObjectInfo;
                 });
         }
@@ -261,7 +246,26 @@ define([
                                             ])
                                         ]),
                                         '<!-- ko if: completionMessage() -->',
-                                        tr([td(['']), td(div({dataBind: {html: 'completionMessage'}}))]),
+                                        tr([td(['']), td(
+                                            div([
+                                                'Successfully copied this data object to the Narrative ',
+                                                span({dataBind: {text: 'completionMessage().title' }, style: {marginRight: '0.5em', fontStyle: 'italic'}}),
+                                                span({style: {fontStyle: 'italic'}}, [ 
+                                                    a(
+                                                        {
+                                                            dataBind: {
+                                                                attr: {
+                                                                    href: 'completionMessage().url'
+                                                                }
+                                                            },
+                                                            class: 'btn btn-default',
+                                                            target: '_blank'
+                                                        },
+                                                        'Open this Narrative'
+                                                    )
+                                                ])
+                                            ])
+                                        )]),
                                         '<!-- /ko -->'
                                     ])
                                 ])
@@ -389,13 +393,17 @@ define([
             viewModel.copyMethod('new');
             ko.applyBindings(viewModel, container);
             return getWritableNarratives(params).then((narratives) => {
-                narratives.forEach((narrative) => {
-                    viewModel.narrativesById[narrative.id] = narrative;
-                    viewModel.narratives.push({
-                        name: narrative.metadata.narrative_nice_name,
-                        value: [String(narrative.id), narrative.metadata.narrative].join('/')
+                narratives
+                    .sort((a, b) => {
+                        return a.metadata.narrative_nice_name.localeCompare(b.metadata.narrative_nice_name);
+                    })
+                    .forEach((narrative) => {
+                        viewModel.narrativesById[narrative.id] = narrative;
+                        viewModel.narratives.push({
+                            name: narrative.metadata.narrative_nice_name,
+                            value: [String(narrative.id), narrative.metadata.narrative].join('/')
+                        });
                     });
-                });
             });
         }
 
