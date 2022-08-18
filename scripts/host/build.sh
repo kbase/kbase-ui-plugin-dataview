33f8b26e05d5761e2d76edac7145d8dec3295530
Erik Pearson <eapearson@lbl.gov>
2022-08-17 18:05:29 -0700

    add echo comments and fix env var [UIP-14]

--- scripts/host/build.sh
+++ scripts/host/build.sh
@@ -1,12 +1,19 @@
-
 export XDG_CONFIG_HOME=${PWD}
 export DIR=${PWD}
 cd tools/node
+echo "Installing packages..."
 docker compose run --rm node npm install
+echo "Cleaning ..."
 docker compose run --rm node npm run clean
+echo "Installing packages..."
 docker compose run --rm node npm install
-docker compose run --rm node npm run -e XDG_CONFIG_HOME=${XDG_CONFIG_HOME} install-bower
+echo "Installing bower packages..."
+docker compose run --rm  -e XDG_CONFIG_HOME=${XDG_CONFIG_HOME} node npm run install-bower
+echo "Copying npm packages..."
 docker compose run --rm node npm run install-npm
+echo "Removing source maps..."
 docker compose run --rm node npm run remove-source-maps
+echo "Preparing dist..."
 docker compose run --rm node npm run install-dist
+echo "DONE!"
 cd ../..
