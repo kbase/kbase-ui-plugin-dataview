13472504873f3a0d5d132a1f54948b84277d5984
Erik Pearson <eapearson@lbl.gov>
2022-08-17 17:36:36 -0700

    try npm path for global persistent config [UIP-14]

--- scripts/host/build.sh
+++ scripts/host/build.sh
@@ -1,9 +1,12 @@
+
+export XDG_CONFIG_HOME=${PWD}
+export DIR=${PWD}
 cd tools/node
-DIR=`pwd`/../.. docker compose run --rm node npm install
-DIR=`pwd`/../.. docker compose run --rm node npm run clean
-DIR=`pwd`/../.. docker compose run --rm node npm install
-DIR=`pwd`/../.. docker compose run --rm node npm run install-bower
-DIR=`pwd`/../.. docker compose run --rm node npm run install-npm
-DIR=`pwd`/../.. docker compose run --rm node npm run remove-source-maps
-DIR=`pwd`/../.. docker compose run --rm node npm run install-dist
+docker compose run --rm node npm install
+docker compose run --rm node npm run clean
+docker compose run --rm node npm install
+docker compose run --rm node npm run install-bower
+docker compose run --rm node npm run install-npm
+docker compose run --rm node npm run remove-source-maps
+docker compose run --rm node npm run install-dist
 cd ../..
