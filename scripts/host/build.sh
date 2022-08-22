e263b4f6b3b3d0b018e79ead3f14979c984c07d9
Erik Pearson <eapearson@lbl.gov>
2022-08-22 09:04:35 -0700

    that worked, now add cache config

--- scripts/host/build.sh
+++ scripts/host/build.sh
@@ -1,6 +1,7 @@
 set -e
-mkdir __config__
-export XDG_CONFIG_HOME=${PWD}/__config__
+rm -rf __xdg__
+mkdir -p __xdg__/confg 
+mkdir -p __xdg__/cache
 export DIR=${PWD}
 echo "--------------------------"
 echo "Starting Build"
@@ -25,7 +26,7 @@ echo "--------------------------"
 echo "Installing bower packages..."
 echo "CONFIG $XDG_CONFIG_HOME"
 # docker compose run --rm  -e XDG_CONFIG_HOME=${XDG_CONFIG_HOME} node npm run install-bower
-docker compose run --rm  -e XDG_CONFIG_HOME=/app/__config__ node npm run install-bower
+docker compose run --rm  -e XDG_CONFIG_HOME=/app/__xdg__/config -e XDG_CACHE_HOME=/app/__xdg__/cache node npm run install-bower
 echo "--------------------------"
 echo "Copying npm packages..."
 docker compose run --rm node npm run install-npm
